e938a63a7462c4ff44c7f27be52242826faa5514
Arrokin <Nicolas.SANANIKONE@student.umons.ac.be>
2019-05-09 23:10:54 +0200

    Additive increase implementation

--- src/reso/examples/selectiverepeat/AdditiveIncrease.java
+++ src/reso/examples/selectiverepeat/AdditiveIncrease.java
@@ -2,8 +2,14 @@ package reso.examples.selectiverepeat;
 
 public class AdditiveIncrease extends CongestionControl {
 
+    double size = protocol.getReceiveWindow().size;
     @Override
     public void control() {
-
+        double tmp = 1/size;
+        size += tmp;
+        if ((int) size  >protocol.getReceiveWindow().size )
+        {
+            protocol.getReceiveWindow().size = (int )size;
+        }
     }
 }
